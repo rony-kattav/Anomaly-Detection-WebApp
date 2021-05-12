c5e8afe35115aa2d2ce348e0afdfdd4c662653bb
noalevy17 <50806363+noalevy17@users.noreply.github.com>
2021-05-12 12:31:32 +0300

    server work

--- expServer.js
+++ expServer.js
@@ -2,8 +2,6 @@ const express = require('express')
 const fileUpload = require('express-fileupload')
 //const model = require('../Model/DetectAnomalies__')
 const model = require('../Model/DetectAnomalies')
-const fs = require('fs');
-const fsPromises = fs.promises;
 
 const app = express()
 app.use(express.urlencoded({
@@ -12,91 +10,39 @@ app.use(express.urlencoded({
 app.use(fileUpload())
 app.use(express.static('../View'))
 
+
 // when opening webbrowser with localhost:8080 it will open the view of the app with the form.
 app.get("/", (req, res) => {
-    res.sendFile("./index.html")
+    //res.sendFile("./noa.html")
+    res.sendFile("../View/public/noa.html")
+    console.log("noa")
 })
 
-// async function createFileAndClose() {
-//     let filehandle = null;
-//     //console.log(file_name +'\n' +content)
-//     try {
-//         // Using the filehandle method 
-//         filehandle = await fsPromises
-//                 .open('learn_file.txt', 'w');
-  
-//         filehandle.writeFile("rony");
-  
-//         filehandle.close();
-//         console.log("File Closed!");
-  
-//     } catch (e) {
-//         console.log("Error", e);
-//     }
-// }
-  
-// // createFileAndClose().catch((error) => {
-// //     console.log("Error", error)
-// // });
+// app.get("/result", (req, res) => {
+//     res.sendFile("./resultView.html")
+// })
 
-async function readThenClose() {
-    let filehandle = null;
-  
-    try {
-        // Using the filehandle method 
-        filehandle = await fsPromises
-                .open('input.txt', 'w');
-                  
-        filehandle.writeFile("hello shaked!")
-  
-        filehandle.close();
-        console.log("File Closed!");
-  
-    } catch (e) {
-        console.log("Error", e);
-    }
-}
-  
-readThenClose().catch((error) => {
-    console.log("Error", error)
-});
 
 // when sending a POST with /detect it will get the result from the model and send it back to the view.
 app.post("/detect", (req, res) => {
+
     let algorithm = req.body.algorithm
     if(req.files) {
         // get the files from the request
         let learn_file = req.files.learn_file
         let detect_file = req.files.detect_file
-
-        // create files with the same content as the files given
-        //createFileAndClose('learn_file.csv', learn_file.data.toString())
-        //createFileAndClose('learn_file.txt', 'rony')
-        //createFileAndClose()
-
-        readThenClose()
-        var learn_file_path = '../../Controller/learn_file.csv'
-
-        // fs.writeFile('./mynewfile2.txt', 'Hello content2!', function (err) {
-        //     if (err) throw err;
-        //     console.log('Saved!');
-        //   });
-
-        var detect_file_path = '../../Controller/detect_file.csv'
         
         // send the model the pathes to the files
-        let result = model.detect(algorithm, learn_file_path , detect_file_path)
-
-        // delete the created files
-        fs.unlink('./learn_file.csv', function (err) {
-            if (err) throw err;
-          });
-        fs.unlink('./detect_file.csv', function (err) {
-        if (err) throw err;
-        });
-
+        let result = model.detect(algorithm, learn_file.data.toString() , detect_file.data.toString())
+        req.anomalies = result
+        
+        //res.redirect('/result')
+        //res.render('resultView.html')
+        // if a callback is specified, the rendered HTML string has to be sent explicitly
+        app.use(express.static('../View'))
+        res.sendFile('./resultView.html')
         // write the result
-        res.write(result)
+        //res.write(req.anomalies)
     }
     res.end()
 })
